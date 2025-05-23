3484fe65d9576846327859f39af18b984356a9ec
dha <106293208+dharesan@users.noreply.github.com>
2025-05-24 01:09:56 +0800

    Create file

---
+++ 2704-ToBeOrNotToBe/2704-ToBeOrNotToBe.js
@@ -0,0 +1,28 @@
+// Last updated: 24/05/2025, 01:09:56
+/**
+ * @param {string} val
+ * @return {Object}
+ */
+var expect = function(val) {
+    var toBe = function(val2) { 
+        if (val === val2) { 
+            return true; 
+        } 
+        throw new Error ("Not Equal")
+    };
+    
+    var notToBe = function(val2){
+        if (val !== val2) { 
+            return true;
+        }
+        throw new Error("Equal")
+    };
+
+    return {toBe,notToBe};
+
+};
+
+/**
+ * expect(5).toBe(5); // true
+ * expect(5).notToBe(5); // throws "Equal"
+ */

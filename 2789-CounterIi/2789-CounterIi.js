9a3ee747549c8a558d2c30e2a6df1cb4981fa515
dha <106293208+dharesan@users.noreply.github.com>
2025-05-24 12:53:13 +0800

    Create 2789-CounterIi

---
+++ 2789-CounterIi/2789-CounterIi.js
@@ -0,0 +1,32 @@
+// Last updated: 24/05/2025, 12:53:13
+/**
+ * @param {integer} init
+ * @return { increment: Function, decrement: Function, reset: Function }
+ */
+var createCounter = function(init) {
+    let count = init; 
+    const original = init; 
+
+    var increment = function(){ 
+        return ++count; 
+    }
+
+    var decrement = function (){
+        return --count; 
+    }
+
+    var reset = function (){ 
+        count = original; 
+        return original; 
+    }
+    
+    return {increment,decrement, reset}; 
+    
+};
+
+/**
+ * const counter = createCounter(5)
+ * counter.increment(); // 6
+ * counter.reset(); // 5
+ * counter.decrement(); // 4
+ */

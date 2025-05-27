73cd8f68b91f1ee802733048af04ec64423743d3
dha <106293208+dharesan@users.noreply.github.com>
2025-05-27 13:44:56 +0800

    Create 2733-Sleep

---
+++ 2733-Sleep/2733-Sleep.js
@@ -0,0 +1,34 @@
+// Last updated: 27/05/2025, 13:44:56
+/**
+ * @param {number} millis
+ * @return {Promise}
+ */
+async function sleep(millis) {
+
+    // var delay = (millis) => {
+    //     var date = new Date();
+    //     while (new Date() - date < millis) { }
+    // };
+
+
+    return new Promise ((resolve) => { 
+        setTimeout (resolve, millis);
+        // resolve("anything")
+
+    }); 
+
+
+    // let t = Date.now();
+    // sleep(millis).then(() => {
+    //     new Promise ((resolve, reject) => { 
+    //         resolve(1); 
+    //     });
+    //     // Date.now() - t); // 100
+    // });
+    
+}
+
+/** 
+ * let t = Date.now()
+ * sleep(100).then(() => console.log(Date.now() - t)) // 100
+ */
